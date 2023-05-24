23049e73f98de02f77df39655afe2265ff3d7f1e
Cpatrici23 <claudia.munozgarcia@gmail.com>
2023-05-24 22:00:34 +0200

    componente playa

--- airbnb-project/src/app/components/playa/playa.component.ts
+++ airbnb-project/src/app/components/playa/playa.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IVivienda } from '../models/vivienda.model';
 
 @Component({
   selector: 'app-playa',
   templateUrl: './playa.component.html',
   styleUrls: ['./playa.component.css']
 })
-export class PlayaComponent {
+export class PlayaComponent  implements OnInit{
+  
+  viviendas: IVivienda[] = []
+  
+  
+  ngOnInit(): void {
+    this.viviendas = [
+      {isAvailable: true, id:1 ,img:'../../../assets/img/1imgplaya.webp',title:'Azulprofundo'},
+      {isAvailable:false, id:2 ,img: '../../../assets/img/2imgplaya.webp',title:'Azulclarito'},
+      {isAvailable:true, id:3 ,img: '../../../assets/img/3imgplaya.webp',title:'Azulaguamarina'},
+      
+
+    ]
+  }
 
 }
+
